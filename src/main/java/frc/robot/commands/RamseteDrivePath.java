1a6fa6694356d5dc440f14306a52755ce73a3083
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2023-01-28 14:00:02 -0800

    Added dashboard values and expanded RamseteDriverPath

---
+++ src/main/java/frc/robot/commands/RamseteDrivePath.java
@@ -0,0 +1,71 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.Constants;
+import frc.robot.subsystems.Drive;
+
+public class RamseteDrivePath extends RamseteCommand {
+  /** Creates a new DriveRamsetePath. */
+  private Drive m_drive;
+  private Trajectory m_trajectory;
+  private boolean m_resetOdometry;
+
+  public RamseteDrivePath(Trajectory trajectory, boolean resetOdometry, Drive drive) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    // alternate construction omits feedfwd, wheelspeeds, and pidcontrollers
+    // two outputs - the simple (non PIDController) sends wheel speeds, not volts!!!
+    // This needs testing on the ground somehow.
+    // PIDController needs serious tuning and testing
+    super(trajectory,
+        drive::getPos,
+        new RamseteController(),
+        // drive.getSmpFeedFwd(),
+        Constants.DriveConstants.kDRIVE_KINEMATICS,
+        // drive::getWheelSpeeds,
+        // new PIDController(DriveNormSubsystemConst.kRAMSETE_P, 0,
+        // DriveNormSubsystemConst.kRAMSETE_D),
+        // new PIDController(DriveNormSubsystemConst.kRAMSETE_P, 0,
+        // DriveNormSubsystemConst.kRAMSETE_D),
+        drive::tankDriveVolts,
+        drive);
+    m_trajectory = trajectory;
+    m_resetOdometry = resetOdometry;
+    m_drive = drive;
+  }
+
+  public RamseteDrivePath(Trajectory trajectory, Drive drive) {
+    this(trajectory, false, drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    super.initialize();
+    if (m_resetOdometry) {
+      m_drive.resetOdometry(m_trajectory.getInitialPose());
+    }
+  }
+  /*
+   * use super inherited methods
+   * // Called every time the scheduler runs while the command is scheduled.
+   * 
+   * @Override
+   * public void execute() {}
+   * // Called once the command ends or is interrupted.
+   * 
+   * @Override
+   * public void end(boolean interrupted) {}
+   * // Returns true when the command should end.
+   * 
+   * @Override
+   * public boolean isFinished() {
+   * return false;
+   * }
+   */
+}
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,11 +4,18 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.kauailabs.navx.frc.AHRS;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
@@ -21,7 +28,14 @@ public class DriveSubsystem extends SubsystemBase {
   private CANSparkMax m_leftController2;
   private CANSparkMax m_rightController1;
   private CANSparkMax m_rightController2;
-  private DifferentialDrive  m_differentialdrive;
+  private DifferentialDrive m_differentialdrive;
+  private AHRS m_navX;
+  private RelativeEncoder m_leftEncoder1;
+  private RelativeEncoder m_rightEncoder1;
+  private double m_leftEncoderSign;
+  private double m_rightEncoderSign;
+  private DifferentialDriveOdometry m_diffOdometry;
+  private double m_driveDistance;
 
   public DriveSubsystem() {
     m_leftController1 = new CANSparkMax(DriveConstants.kLeftMotor1Port, MotorType.kBrushless);
@@ -48,7 +62,7 @@ public class DriveSubsystem extends SubsystemBase {
     m_leftController2.setSmartCurrentLimit(DriveConstants.kCURRENT_LIMT);
     m_rightController1.setSmartCurrentLimit(DriveConstants.kCURRENT_LIMT);
     m_rightController2.setSmartCurrentLimit(DriveConstants.kCURRENT_LIMT);
-    
+
     m_leftController2.follow(m_leftController1);
     m_rightController2.follow(m_rightController1);
     m_differentialdrive = new DifferentialDrive(m_leftController1, m_rightController1);
@@ -57,10 +71,54 @@ public class DriveSubsystem extends SubsystemBase {
     // super(
     // The PIDController used by the subsystem
     // new PIDController(0, 0, 0));
+    m_leftEncoder1 = m_leftController1.getEncoder();
+    m_rightEncoder1 = m_rightController1.getEncoder();
+    m_diffOdometry = new DifferentialDriveOdometry(new Rotation2d(), m_driveDistance, m_driveDistance);
+    m_navX = new AHRS(SerialPort.Port.kMXP);
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Yaw", getYaw());
+    SmartDashboard.putNumber("Pitch", getPitch());
+    SmartDashboard.putNumber("Roll", getRoll());
+    SmartDashboard.putNumber("Velocity", getVelocity());
+    SmartDashboard.putNumber("Left Distance", getLeftDistance());
+    SmartDashboard.putNumber("Right Distance", getRightDistance());
+    // System.out.println(getPitch());
+
+  }
+
+  public Pose2d getPos() {
+    return  m_diffOdometry.getPoseMeters();
+
+  }
+
+  public void resetOdometry(Pose2d pose) {
+    resetEncoders();
+    m_diffOdometry.resetPosition(new Rotation2d(0), m_leftController1.getEncoder().getPosition(),
+        m_rightController1.getEncoder().getPosition(), pose);
+  }
+
+  public double getYaw() {
+    return m_navX.getYaw();
+  }
+
+  public double getPitch() {
+    return m_navX.getPitch();
+  }
+
+  public double getRoll() {
+    return m_navX.getRoll();
+  }
+
+  public double getVelocity() {
+    return m_navX.getVelocityY();
+  }
+
+  public double getAngleK() {
+
+    return m_navX.getAngle();
   }
 
   public void arcadeDrive(double velocity, double heading) {
@@ -68,4 +126,42 @@ public class DriveSubsystem extends SubsystemBase {
     // System.out.println("velocity="+velocity);
     // System.out.println("heading="+heading);
   }
+
+  public void tankDriveVolts(double leftVolt, double rightVolt) {
+    m_leftController1.setVoltage(leftVolt);
+    m_rightController1.setVoltage(rightVolt);
+    m_differentialdrive.feed();
+  }
+
+  private Rotation2d getRotation2dK() {
+    // note the negation of the angle is required because the wpilib convention
+    // uses left positive rotation while gyros read right positive
+    return Rotation2d.fromDegrees(-getAngleK());
+  }
+
+  public void resetDrive() {
+    resetAngle();
+    resetEncoders();
+  }
+
+  private void resetAngle() {
+    m_navX.zeroYaw();
+    // need to add reset of odometry and encoders
+  }
+
+  private double getLeftDistance() {
+    return m_leftEncoderSign * m_leftEncoder1.getPosition() * DriveConstants.kENCODER_DISTANCE_PER_PULSE_M;
+  }
+
+  private double getRightDistance() {
+    return m_rightEncoder1.getPosition() * DriveConstants.kENCODER_DISTANCE_PER_PULSE_M;
+    // return m_rightEncoderSign * m_rightEncoder1.getPosition() *
+    // DriveConstants.kENCODER_DISTANCE_PER_PULSE_M;
+  }
+
+  private void resetEncoders() {
+    m_leftEncoder1.setPosition(0);
+    m_rightEncoder1.setPosition(0);
+  }
+
 }
