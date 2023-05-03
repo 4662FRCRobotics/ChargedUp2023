e832bf7321cf7df4ec20af7b746bf767359aa0a6
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2023-05-02 18:02:45 -0700

    message

--- src/main/java/frc/robot/subsystems/AutonomousSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutonomousSubsystem.java
@@ -280,8 +280,8 @@ public class AutonomousSubsystem extends SubsystemBase {
   private void genTrajectory() {
     m_drive3Trajectory = TrajectoryGenerator.generateTrajectory(
         new Pose2d(0, 0, new Rotation2d(0)),
-        List.of(new Translation2d(1.5, 0)),
-        new Pose2d(2.5, 0, new Rotation2d(0)),
+        List.of(new Translation2d(-1.5, 0)),
+        new Pose2d(-2.5, 0, new Rotation2d(0)),
         m_drive.getTrajConfig());
   }
   // change posistions to constants at home
